c94e37837a1ab6ad09a4756bfe4c943976a0d1e1
Ashley <ashleyh5440@gmail.com>
2023-10-04 18:24:41 -0400

    added accordian panel to css flexbox section

--- assets/script.js
+++ assets/script.js
@@ -25,6 +25,22 @@ listTopics()
 console.log('Which topic should we study first?');
 selectTopic()
 
+// for flexbox accordian
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 
 
 
