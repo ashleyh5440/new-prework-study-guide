12569cdf7fdfaacb54b8172e4e7237afd805ed5c
Ashley <ashleyh5440@gmail.com>
2023-11-14 21:17:05 -0500

    added styling to html html page, started functionality

---
+++ assets/js/cards-script.js
@@ -0,0 +1,115 @@
+//when a box is clicked, the initial section disappears and the corresponding box section appears
+
+const box1 = document.getElementById("box1")
+.addEventListener('click', function() {
+  
+})
+
+const box2 = document.getElementById("box2")
+.addEventListener('click', function() {
+  
+})
+
+const box3 = document.getElementById("box3")
+.addEventListener('click', function() {
+
+})
+
+
+
+
+
+
+
+
+// for flexbox accordian
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+ 
+
+
+
+
+
+// var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+// var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+// function listTopics() {
+//   for (var x = 0; x < topics.length; x++) {
+//     console.log(topics[x]);
+//   }
+// }
+
+// function selectTopic() {
+// if (randomTopic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (randomTopic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (randomTopic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (randomTopic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+// }
+// console.log('Here are the topics we learned through Prework:');
+// listTopics()
+// console.log('Which topic should we study first?');
+// selectTopic()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+const card3 = document.getElementById("card3")
+.addEventListener('click', function() {
+  window.location.href = 'html.html'
+})
+
+const card4 = document.getElementById("card4")
+.addEventListener('click', function() {
+  window.location.href = 'css.html'
+})
+
+const card5 = document.getElementById("card5")
+.addEventListener('click', function() {
+  window.location.href = 'javascript.html'
+})
+
+
+
+
+
+
+
+
+// for flexbox accordian
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+ 
+
+
+
+
+
+// var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+// var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+// function listTopics() {
+//   for (var x = 0; x < topics.length; x++) {
+//     console.log(topics[x]);
+//   }
+// }
+
+// function selectTopic() {
+// if (randomTopic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (randomTopic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (randomTopic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (randomTopic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+// }
+// console.log('Here are the topics we learned through Prework:');
+// listTopics()
+// console.log('Which topic should we study first?');
+// selectTopic()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
